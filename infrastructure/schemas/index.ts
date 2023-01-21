d9b53cb2d6b02ef2e54b65bcab5965032b2eb2c2
Girish Patil <girish.patil@gosource.com.au>
2023-01-21 11:03:21 +0000

    Merged PR 312: fix: pulumi dependencies

--- infrastructure/schemas/index.ts
+++ infrastructure/schemas/index.ts
@@ -11,43 +11,53 @@ export const schemasBucket = new aws.s3.Bucket(
   }
 );
 
-new synced.S3BucketFolder(`dvp-${process.env.ENV}-sync-schemas`, {
-  path: '../artifacts/schemas',
-  bucketName: schemasBucket.bucket,
-  acl: aws.s3.PrivateAcl,
-});
-
-export const schemasBucketNotificationTopic = new aws.sqs.Queue(
-  `dvp-${process.env.ENV}-credential-schemas-event-queue`,
+export const syncedS3BucketFolder = new synced.S3BucketFolder(
+  `dvp-${process.env.ENV}-sync-schemas`,
   {
-    policy: pulumi.interpolate`{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": "*",
-      "Action": "sqs:SendMessage",
-	  "Resource": "arn:aws:sqs:*:*:s3-event-notification-queue",
-      "Condition": {
-        "ArnEquals": { "aws:SourceArn": "${schemasBucket.arn}" }
-      }
-    }
-  ]
-}
-`,
+    path: '../artifacts/schemas',
+    bucketName: pulumi.interpolate`${schemasBucket.bucket}`,
+    acl: aws.s3.PrivateAcl,
+  },
+  {
+    dependsOn: [schemasBucket],
   }
 );
 
+export const schemasBucketNotificationQueue = new aws.sqs.Queue(
+  `dvp-${process.env.ENV}-credential-schemas-event-queue`,
+  {
+    policy: pulumi.interpolate`{
+      "Version": "2012-10-17",
+      "Statement": [
+        {
+          "Effect": "Allow",
+          "Principal": "*",
+          "Action": "sqs:SendMessage",
+          "Resource": "arn:aws:sqs:*:*:*",
+          "Condition": {
+            "ArnEquals": { "aws:SourceArn": "${schemasBucket.arn}" }
+          }
+        }
+      ]
+    }`,
+  },
+  {
+    dependsOn: [syncedS3BucketFolder],
+  }
+);
 export const schemasBucketNotification = new aws.s3.BucketNotification(
   `dvp-${process.env.ENV}-credential-schemas-notification`,
   {
     bucket: schemasBucket.id,
-    topics: [
+    queues: [
       {
-        topicArn: schemasBucketNotificationTopic.arn,
+        queueArn: pulumi.interpolate`${schemasBucketNotificationQueue.arn}`,
         events: ['s3:ObjectCreated:*', 's3:ObjectRemoved:*'],
         filterSuffix: '.json',
       },
     ],
+  },
+  {
+    dependsOn: [schemasBucketNotificationQueue],
   }
 );
