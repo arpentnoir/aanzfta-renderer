20659e4924aed05a1e0577433fde14223d41b38a
Girishgouda PATIL <XH36ML@DIN.BCZ.GOV.AU>
2023-05-23 23:46:38 +0000

    Merged PR 6027: feat: update schema sync to use native aws cli
    Moved to using the aws cli for syncing the schemas, so we can get bucket notifications for every deployment.
    
    Related work items: #23165

--- infrastructure/schemas/index.ts
+++ infrastructure/schemas/index.ts
@@ -1,14 +1,13 @@
 import * as aws from '@pulumi/aws';
+import * as command from '@pulumi/command';
 import * as pulumi from '@pulumi/pulumi';
-import * as synced from '@pulumi/synced-folder';
-
-const dvpStack = pulumi.getStack();
 
 export const schemasBucket = new aws.s3.Bucket(
   `dvp-${process.env.ENV}-credential-schemas`,
   {
     bucket: `dvp-${process.env.ENV}-credential-schemas`,
     acl: aws.s3.PrivateAcl,
+    forceDestroy: true,
   }
 );
 
@@ -53,12 +52,24 @@ export const schemasBucketNotification = new aws.s3.BucketNotification(
   }
 );
 
-export const syncedS3BucketFolder = new synced.S3BucketFolder(
-  `dvp-${process.env.ENV}-sync-schemas`,
+const schemasPath = '../artifacts/schemas';
+const schemasBucketName = pulumi.interpolate`${schemasBucket.bucket}`;
+const syncedTimeStamp = new Date().toISOString();
+const syncCommand = pulumi.interpolate`
+aws s3 sync "${schemasPath}" "s3://${schemasBucketName}" --acl "${aws.s3.PrivateAcl}" --region "${process.env.AWS_REGION}" --delete --only-show-errors;
+aws s3api list-objects --bucket "${schemasBucketName}" --region "${process.env.AWS_REGION}" --query 'Contents[].{Key:Key}' --output text | xargs -n 1 -I targetobject aws s3api put-object-tagging --bucket "${schemasBucketName}" --region "${process.env.AWS_REGION}" --tagging 'TagSet=[{Key="LAST_SYNCED_TIMESTAMP",Value="${syncedTimeStamp}"}]' --key targetobject;
+`;
+const deleteCommand = pulumi.interpolate`aws s3 rm "s3://${schemasBucketName}" --include "*" --recursive --only-show-errors`;
+
+new command.local.Command(
+  `dvp-${process.env.ENV}-sync-schemas-command`,
   {
-    path: '../artifacts/schemas',
-    bucketName: pulumi.interpolate`${schemasBucket.bucket}`,
-    acl: aws.s3.PrivateAcl,
+    create: syncCommand,
+    update: syncCommand,
+    delete: deleteCommand,
+    environment: {
+      LAST_SYNCED_TIMESTAMP: syncedTimeStamp,
+    },
   },
   {
     dependsOn: [schemasBucketNotification],
